2105134957bca22010181939f22d91fe633425ce
Iqbal Reza Puteh <99324365+IqbalPuteh@users.noreply.github.com>
2025-07-20 12:54:24 +0700

    Update config.py
    remove password

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 LOGIN_URL="https://cbsnusantara.cbclik.com/Account/Login"
 USERNAME ="NEFO4081"
-PASSWORD ="Ptfti2025"
+PASSWORD ="password"
 BASE_URL = "https://cbsnusantara.cbclik.com/"
 HEADLESS = False
